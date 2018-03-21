46f217a59110bb04ee77b46a8f4f2fa9b7425fcc
genrich <genrich.ivaska@epfl.ch>
2018-03-21 11:32:05 +0100

    update schema loading to staging script
    Change-Id: I3b024e051ca1b32b23dc742aa916de47432f0f7c

--- scripts/schemas.sh
+++ scripts/schemas.sh
@@ -1,40 +1,43 @@
-BASE=https://bbp-nexus.epfl.ch/dev
+BASE=https://bbp-nexus.epfl.ch/staging
 
 http -v -j PUT $BASE/v0/organizations/neurosciencegraph <<< {"@context":{"schema":"http://schema.org/"},"schema:name":"neurosciencegraph"}
 http -v -j PUT $BASE/v0/domains/neurosciencegraph/simulation 'Accept:application/ld+json' description='Simulation domain'
 
-
-
 http -v -j PUT $BASE/v0/domains/neurosciencegraph/commons 'Accept:application/ld+json' description='Commons domain'
+
 http PUT $BASE/v0/schemas/neurosciencegraph/commons/entity/v0.1.0 'Accept:application/ld+json' < modules/commons/target/exported/neurosciencegraph/commons/entity/v0.1.0.json
 http PATCH $BASE/v0/schemas/neurosciencegraph/commons/entity/v0.1.0/config rev==1 published:=true 'Accept:application/ld+json'
+
+http PUT $BASE/v0/schemas/neurosciencegraph/commons/modelinstance/v0.1.1 'Accept:application/ld+json' < modules/commons/target/exported/neurosciencegraph/commons/modelinstance/v0.1.1.json
+http PATCH $BASE/v0/schemas/neurosciencegraph/commons/modelinstance/v0.1.1/config rev==1 published:=true 'Accept:application/ld+json'
 
 http -v -j PUT $BASE/v0/domains/neurosciencegraph/core 'Accept:application/ld+json' description='Core domain'
 http PUT $BASE/v0/contexts/neurosciencegraph/core/data/v0.1.0 'Accept:application/ld+json' < modules/commons/target/exported/contexts/v0.1.0.json
 http PATCH $BASE/v0/contexts/neurosciencegraph/core/data/v0.1.0/config rev==1 published:=true 'Accept:application/ld+json'
 
-
-
 http PUT $BASE/v0/schemas/neurosciencegraph/simulation/morphologyrelease/v0.1.0 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/morphologyrelease/v0.1.0.json
 http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/morphologyrelease/v0.1.0/config rev==1 published:=true 'Accept:application/ld+json'
-
-http PUT $BASE/v0/schemas/neurosciencegraph/simulation/modelinstance/v0.1.0 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/modelinstance/v0.1.0.json
-http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/modelinstance/v0.1.0/config rev==1 published:=true 'Accept:application/ld+json'
 
 http PUT $BASE/v0/schemas/neurosciencegraph/simulation/modelscript/v0.1.0 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/modelscript/v0.1.0.json
 http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/modelscript/v0.1.0/config rev==1 published:=true 'Accept:application/ld+json'
 
-http PUT $BASE/v0/schemas/neurosciencegraph/simulation/subcellularmodel/v0.1.0 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/subcellularmodel/v0.1.0.json
-http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/subcellularmodel/v0.1.0/config rev==1 published:=true 'Accept:application/ld+json'
+http PUT $BASE/v0/schemas/neurosciencegraph/simulation/subcellularmodelscript/v0.1.0 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/subcellularmodelscript/v0.1.0.json
+http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/subcellularmodelscript/v0.1.0/config rev==1 published:=true 'Accept:application/ld+json'
 
-http PUT $BASE/v0/schemas/neurosciencegraph/simulation/emodel/v0.1.0 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/emodel/v0.1.0.json
-http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/emodel/v0.1.0/config rev==1 published:=true 'Accept:application/ld+json'
+http PUT $BASE/v0/schemas/neurosciencegraph/simulation/subcellularmodel/v0.1.1 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/subcellularmodel/v0.1.1.json
+http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/subcellularmodel/v0.1.1/config rev==1 published:=true 'Accept:application/ld+json'
+
+http PUT $BASE/v0/schemas/neurosciencegraph/simulation/emodel/v0.1.1 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/emodel/v0.1.1.json
+http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/emodel/v0.1.1/config rev==1 published:=true 'Accept:application/ld+json'
 
 http PUT $BASE/v0/schemas/neurosciencegraph/simulation/emodelrelease/v0.1.0 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/emodelrelease/v0.1.0.json
 http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/emodelrelease/v0.1.0/config rev==1 published:=true 'Accept:application/ld+json'
 
-http PUT $BASE/v0/schemas/neurosciencegraph/simulation/morphology/v0.1.0 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/morphology/v0.1.0.json
-http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/morphology/v0.1.0/config rev==1 published:=true 'Accept:application/ld+json'
+http PUT $BASE/v0/schemas/neurosciencegraph/simulation/morphology/v0.1.1 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/morphology/v0.1.1.json
+http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/morphology/v0.1.1/config rev==1 published:=true 'Accept:application/ld+json'
+
+http PUT $BASE/v0/schemas/neurosciencegraph/simulation/memodel/v0.1.1 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/memodel/v0.1.1.json
+http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/memodel/v0.1.1/config rev==1 published:=true 'Accept:application/ld+json'
 
 http PUT $BASE/v0/schemas/neurosciencegraph/simulation/memodelrelease/v0.1.0 'Accept:application/ld+json' < modules/simulation/target/exported/neurosciencegraph/simulation/memodelrelease/v0.1.0.json
 http PATCH $BASE/v0/schemas/neurosciencegraph/simulation/memodelrelease/v0.1.0/config rev==1 published:=true 'Accept:application/ld+json'
