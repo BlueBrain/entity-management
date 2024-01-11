6513d9cd50888b1ef7087eba5f4e3d07f1c6b70d
Joni Herttuainen <joni.herttuainen@epfl.ch>
2024-01-11 16:08:13 +0100

    simplify the CLI, rework model_building_config.py

--- entity_management/cli/model_building_config.py
+++ entity_management/cli/model_building_config.py
@@ -1,85 +1,59 @@
 """Command line interface for Model Building Config."""
 import logging
-from pprint import pprint
+import attr
 from entity_management.config import (
-    MacroConnectomeConfig, ModelBuildingConfig, BrainRegionSelectorConfig
+    MacroConnectomeConfig, BrainRegionSelectorConfig
 )
 from entity_management.simulation import DetailedCircuit
 from entity_management.atlas import CellComposition
 
 
-def get_model_building_config(nexus_url, nexus_id):
-    """Get ModelBuildingConfig instance from Nexus by URL or ID.
+def _used_in_as_dict(used_in, config):
+    result = {"id": used_in.generated.get_id()}
 
-    Args:
-        nexus_url (str): URL of the ModelBuildingConfig
-        nexus_id (str): ID of the ModelBuildingConfig
-    """
-    if (nexus_url and nexus_id) or ((not nexus_url) and (not nexus_id)):
-        raise ValueError("Exactly one of `url` or `id` must be set at a time.")
+    if isinstance(used_in.generated, DetailedCircuit):
+        result["atlas_release_id"] = used_in.generated.atlasRelease.get_id()
+        result["circuit_url"] = used_in.generated.circuitConfigPath.url
+    elif isinstance(used_in.generated, CellComposition):
+        result["atlas_release_id"] = used_in.generated.atlasRelease.get_id()
+        result["cell_composition_summary"] = used_in.generated.cellCompositionSummary.get_id()
+        result["cell_composition_volume"] = used_in.generated.cellCompositionVolume.get_id()
+    elif isinstance(used_in.generated, (MacroConnectomeConfig, BrainRegionSelectorConfig)):
+        pass  # the `generated` points to a clone of the MacroConnectomeConfig
+    else:
+        logging.warning(
+            "Unexpected type of `used_in` in \"%s\": %s (id: %s)",
+            config.name,
+            type(used_in.generated),
+            used_in.generated.get_id(),
+        )
+    return result
 
-    if nexus_url:
-        data = ModelBuildingConfig.from_url(url=nexus_url)
-    if nexus_id:
-        data = ModelBuildingConfig.from_id(resource_id=nexus_id)
 
-    result = {
-        "name": data.name,
-        "description": data.description,
-    }
+def _config_usage_as_list(config):
+    return [_used_in_as_dict(u, config) for u in config.used_in]
 
-    result["configs"] = {}
 
-    for config in (
-        data.configs.cellCompositionConfig,
-        data.configs.cellPositionConfig,
-        data.configs.morphologyAssignmentConfig,
-        data.configs.eModelAssignmentConfig,
-        data.configs.macroConnectomeConfig,
-        data.configs.microConnectomeConfig,
-        data.configs.synapseConfig,
-        data.configs.meModelConfig,
-    ):
-        if config is None:
-            continue
-
-        result["configs"][config.name] = {
+def _configs_as_dict(configs):
+    return {
+        config.name: {
             "name": config.name,
             "description": config.description,
             "generatorName": config.generatorName,
             "content": config.distribution.contentUrl,
-        }
+            "used_in": _config_usage_as_list(config)
+        } for config in configs
+    }
 
-        result["configs"][config.name]["used_in"] = []
-        for used_in in config.used_in:
-            used_in_result = {
-                "id": used_in.generated.get_id(),
-            }
-            if isinstance(used_in.generated, DetailedCircuit):
-                used_in_result[
-                    "atlas_release_id"
-                ] = used_in.generated.atlasRelease.get_id()
-                used_in_result["circuit_url"] = used_in.generated.circuitConfigPath.url
-            elif isinstance(used_in.generated, CellComposition):
-                used_in_result[
-                    "atlas_release_id"
-                ] = used_in.generated.atlasRelease.get_id()
-                used_in_result[
-                    "cell_composition_summary"
-                ] = used_in.generated.cellCompositionSummary.get_id()
-                used_in_result[
-                    "cell_composition_volume"
-                ] = used_in.generated.cellCompositionVolume.get_id()
-            elif isinstance(used_in.generated, (MacroConnectomeConfig, BrainRegionSelectorConfig)):
-                pass  # the `generated` points to a clone of the MacroConnectomeConfig
-            else:
-                logging.warning(
-                    "Unexpected type of `used_in` in \"%s\": %s (id: %s)",
-                    config.name,
-                    type(used_in.generated),
-                    used_in.generated.get_id(),
-                )
 
-            result["configs"][config.name]["used_in"].append(used_in_result)
+def _iter_configs(configs):
+    yield from (c for c in attr.astuple(configs, recurse=False) if c is not None)
 
-    pprint(result)
+
+def model_building_config_as_dict(model_config):
+    """Get ModelBuildingConfig as a dict."""
+    return {
+        "name": model_config.name,
+        "description": model_config.description,
+        "configs": _configs_as_dict(_iter_configs(model_config.configs))
+    }
